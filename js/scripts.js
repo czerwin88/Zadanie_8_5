4b9733b4c9085eef1508789720923cc3c1d8bce6
Czerwin <r.j.czerwinski@gmail.com>
2017-06-02 22:11:51 +0200

    Fixed again

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 var a = 30;
     h = 24;
     triangleArea = a * h / 2;
+    
 alert('Pole trójkąta o podstawie a i wysokości h wynosi' + triangleArea);
-
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-
-
